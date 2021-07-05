d8cd0c2d6c728274da7173d3a4b891884755d452
Rahul Bansal <rahulbansal.btech20@sushantuniversity.edu.in>
2021-07-05 23:35:05 +0530

    python

--- Python/questions/q1.py
+++ Python/questions/q1.py
@@ -1,10 +1,6 @@
-"""
-
-Define a function max() that takes two numbers as arguments and returns the largest of them. 
-Use the if-then-else construct available in Python. 
-(It is true that Python has the max() function built in, but writing it yourself is nevertheless a good exercise.)
-
-"""
+# Define a function max() that takes two numbers as arguments and returns the largest of them. 
+# Use the if-then-else construct available in Python. 
+# (It is true that Python has the max() function built in, but writing it yourself is nevertheless a good exercise.)
 
 a = input("Enter number a: ")
 b = input("Enter number b: ")
@@ -13,12 +9,14 @@ print(max(a, b))
 
 def max(a, b):
 
-    if(a == b):
-        print("Both Values are equal")
     
-    elif(a > b):
+    if(a > b):
         return a
 
+    elif(a == b):
+        # print("Both Values are equal")
+        return 0
+        
     elif(a < b):
         return b
 
